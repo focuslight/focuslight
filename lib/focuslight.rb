183932fe87fe6348f5ca9762c2f30014a81547cc
sonots <sonots@gmail.com>
2014-02-23 16:36:16 +0900

    add a comment about ruby 2.0.0 log rotation

--- lib/focuslight.rb
+++ lib/focuslight.rb
@@ -3,6 +3,8 @@ Dotenv.load
 require "focuslight/version"
 require "focuslight/config"
 require 'logger'
+# NOTE: Please note that ruby 2.0.0's Logger has a problem on log rotation.
+# Update to ruby 2.1.0 to use log rotation. See https://github.com/ruby/ruby/pull/428 for details.
 $logger = Logger.new('log/application.log')
 $logger.level = Focuslight::Config.get(:log_level)
 
