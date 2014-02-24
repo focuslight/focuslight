4feb66f11096ea4c4c66458a6b6cbb50b0285276
sonots <sonots@gmail.com>
2014-02-24 12:00:19 +0900

    Focuslight::Logger::Config

--- lib/focuslight/config.rb
+++ lib/focuslight/config.rb
@@ -13,44 +13,13 @@ module Focuslight::Config
     when :dburl
       ENV.fetch('DBURL', 'sqlite://data/gforecast.db')
     when :log_path
-      case log_path = ENV.fetch('LOG_PATH', DEFAULT_LOG_PATH)
-      when 'STDOUT'
-        $stdout
-      when 'STDERR'
-        $stderr
-      else
-        log_path
-      end
+      ENV.fetch('LOG_PATH', DEFAULT_LOG_PATH)
     when :log_level
-      case log_level = ENV.fetch('LOG_LEVEL', 'info')
-      when 'debug'
-        Logger::DEBUG
-      when 'info'
-        Logger::INFO
-      when 'warn'
-        Logger::WARN
-      when 'error'
-        Logger::ERROR
-      when 'fatal'
-        Logger::FATAL
-      else
-        raise ArgumentError, "invalid log_level #{log_level}"
-      end
+      ENV.fetch('LOG_LEVEL', 'info')
     when :log_shift_age
-      case shift_age = ENV.fetch('LOG_SHIFT_AGE', '0')
-      when /\d+/
-        shift_age.to_i
-      when 'daily'
-        shift_age
-      when 'weekly'
-        shift_age
-      when 'monthly'
-        shift_age
-      else
-        raise ArgumentError, "invalid log_shift_age #{shift_age}"
-      end
+      ENV.fetch('LOG_SHIFT_AGE', '0')
     when :log_shift_size
-      ENV.fetch('LOG_SHIFT_SIZE', '1048576').to_i
+      ENV.fetch('LOG_SHIFT_SIZE', '1048576')
     else
       raise ArgumentError, 'unknown configuration keyword'
     end
